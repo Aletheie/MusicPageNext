3c5c383f153c4ac2aeb5f6c3ef813534abb6c973
Anicka <anna.jungmannova@ssps.cz>
2023-10-17 13:29:54 +0200

    fix: ipod position

--- app/components/ipod.tsx
+++ app/components/ipod.tsx
@@ -3,7 +3,7 @@ import ipod from "../../public/gummy-ipod.svg";
 
 const Ipod = () => {
   return (
-    <div className="hidden md:block w-2/3 md:w-3/5 absolute -left-5 md:left-35 md:bottom-32 lg:bottom-0 lg:top-5 floating">
+    <div className="hidden md:block w-2/3 md:w-3/5 absolute -left-5 md:left-35 md:bottom-32 lg:bottom-28 lg:top-5 floating">
       <Image src={ipod} alt="ipod" layout="fill" objectFit="contain" />
     </div>
   );
