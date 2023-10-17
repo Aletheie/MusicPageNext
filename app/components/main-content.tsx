584572192cb628b9a4b83b447098c47c46a50455
Anicka <anna.jungmannova@ssps.cz>
2023-10-17 12:57:20 +0200

    feat: add and use banner component

--- app/components/main-content.tsx
+++ app/components/main-content.tsx
@@ -1,19 +1,18 @@
-// import Banner from "./Banner";
+import Banner from "./banner";
 // import BottomPlayer from "./BottomPlayer";
 // import Ipod from "./Ipod";
 // import TrackList from "./TrackList";
 
 const MainContent = () => {
   return (
-    <h1>ahoj</h1>
-    // <div className="w-full relative h-screen col-span-12 lg:col-span-10 bg-[#f4f4f4] rounded-l-3xl shadow-md overflow-hidden">
-    //   <Banner />
-    //   <div className="flex flex-col justify-center">
-    //     <Ipod />
-    //   </div>
-    //   <TrackList />
-    //   <BottomPlayer />
-    // </div>
+    <div className="w-full relative h-screen col-span-12 lg:col-span-10 bg-[#f4f4f4] rounded-l-3xl shadow-md overflow-hidden">
+      <Banner />
+      {/* <div className="flex flex-col justify-center">
+        <Ipod />
+    </div>
+    <TrackList />
+      <BottomPlayer /> */}
+    </div>
   );
 };
 
