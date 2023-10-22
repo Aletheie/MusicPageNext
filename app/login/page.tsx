775b58f924b20aff00e390a472960577f9e28a96
Anicka <anna.jungmannova@ssps.cz>
2023-10-22 15:15:53 +0200

    feat: add main login page

---
+++ app/login/page.tsx
@@ -0,0 +1,14 @@
+import LoginInputGroup from "./components/login-input-group";
+import login from "@/public/icons8-login-64.png";
+import signup from "@/public/icons8-access-64.png";
+
+const Login = () => {
+  return (
+    <div className="w-full h-screen col-span-12 lg:col-span-10 bg-[#f4f4f4] rounded-l-3xl shadow-md overflow-hidden flex flex-col lg:flex-row justify-center">
+      <LoginInputGroup icon={login} buttonText="Login" />
+      <LoginInputGroup icon={signup} buttonText="SignUp" oneMoreInput />
+    </div>
+  );
+};
+
+export default Login;
