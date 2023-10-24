cf594666eaa758012d99d8dbf959dd89c5393aff
Anicka <anna.jungmannova@ssps.cz>
2023-10-24 14:34:48 +0200

    feat: add login routing

---
+++ app/api/login/route.ts
@@ -0,0 +1,40 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import { NextResponse } from "next/server";
+import { UserType } from "@/utils/user-type";
+
+const prisma = new PrismaClient();
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  console.log(body || "no body");
+  const { name, email, passwordHash } = body as unknown as UserType;
+
+  if (name) {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(passwordHash, salt);
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        passwordHash: hash,
+      },
+    });
+    return NextResponse.json(newUser);
+  } else {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (user) {
+      const isPasswordCorrect = await bcrypt.compare(
+        passwordHash,
+        user.passwordHash
+      );
+      if (isPasswordCorrect) {
+        return NextResponse.json(user);
+      }
+    }
+  }
+}
