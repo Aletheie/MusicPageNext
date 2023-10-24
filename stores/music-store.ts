292568ddba47732d50e4cc5a1cbb12ab2d92d344
Anicka <anna.jungmannova@ssps.cz>
2023-10-24 09:31:30 +0200

    feat: add zustand music store

---
+++ stores/music-store.ts
@@ -0,0 +1,33 @@
+import { create } from "zustand";
+import { SongType } from "../utils/song-type";
+
+interface Store {
+  isGlobalPlaying: boolean;
+  song: SongType;
+  globalSongList: SongType[];
+  setIsGlobalPlaying: () => void;
+  setSong: (song: SongType) => void;
+  setGlobalSongList: (songList: SongType[]) => void;
+}
+
+const useMusicStore = create<Store>((set) => ({
+  isGlobalPlaying: false,
+  song: {
+    songName: "",
+    songAuthor: "",
+    _id: "",
+    isFilledHeart: false,
+    songFile: {
+      name: "",
+      type: "",
+      path: "",
+    },
+  },
+  globalSongList: [],
+  setIsGlobalPlaying: () =>
+    set((state) => ({ isGlobalPlaying: !state.isGlobalPlaying })),
+  setSong: (song) => set(() => ({ song })),
+  setGlobalSongList: (songList) => set(() => ({ globalSongList: songList })),
+}));
+
+export default useMusicStore;
