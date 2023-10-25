8bed5ec8729cb571a88e9f7c496ea4f676fc155d
Anicka <anna.jungmannova@ssps.cz>
2023-10-25 10:42:28 +0200

    feat: define simple post route

--- app/api/songs/route.ts
+++ app/api/songs/route.ts
@@ -1,4 +1,7 @@
 import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const dataSource = "https://jsonplaceholder.typicode.com/todos";
 
@@ -10,5 +13,34 @@ export async function GET() {
 
 export async function POST(request: Request) {
   const body = await request.json();
-  return NextResponse.json(body);
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+    },
+  });
+  if (user == null) return NextResponse.json({ message: "User not found" });
+
+  if (!body.songName)
+    return NextResponse.json({ message: "Song name is required" });
+  if (!body.songAuthor)
+    return NextResponse.json({ message: "Song author is required" });
+  if (!body.songFile)
+    return NextResponse.json({ message: "Song file is required" });
+
+  const newSong = await prisma.song.create({
+    data: {
+      songName: body.songName,
+      songAuthor: user.name,
+      songFile: body.songFile,
+      isFilledHeart: false,
+      user: {
+        connect: {
+          id: user?.id,
+        },
+      },
+    },
+  });
+
+  console.log(newSong);
 }
