9a57afee4b57bdf38efb5cff661a8af21388f4ce
Anicka <anna.jungmannova@ssps.cz>
2023-10-27 18:26:33 +0200

    fix: change outdated props

--- app/components/ipod.tsx
+++ app/components/ipod.tsx
@@ -4,7 +4,7 @@ import ipod from "../../public/gummy-ipod.svg";
 const Ipod = () => {
   return (
     <div className="hidden md:block w-2/3 md:w-3/5 absolute -left-5 md:left-35 md:bottom-32 lg:bottom-28 lg:top-5 floating">
-      <Image src={ipod} alt="ipod" layout="fill" objectFit="contain" />
+      <Image src={ipod} alt="ipod" />
     </div>
   );
 };
