a2a94786218b2e1dacf7269ce25b64a8b680671d
Anicka <anna.jungmannova@ssps.cz>
2023-10-27 16:54:32 +0200

    fix: add config

--- middlewares/middleware.ts
+++ middlewares/middleware.ts
@@ -1,5 +1,9 @@
 import { checkSongLimit } from "./check-song-limit";
 import { stackMiddlewares } from "@/middlewares/stack-handler";
 
+export const config = {
+  matcher: "/api/songs",
+};
+
 const middlewares = [checkSongLimit];
 export default stackMiddlewares(middlewares);
