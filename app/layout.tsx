f70bd158e6c57fbf68053c88b0c5d07a5a0039f7
Anicka <anna.jungmannova@ssps.cz>
2023-10-27 18:00:23 +0200

    fix: correct layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "./components/navbar";
+import NextAuthProvider from "@/app/auth-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,16 +13,20 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  Session,
 }: {
   children: React.ReactNode;
+  Session: any;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <section className="grid grid-cols-12 w-full h-screen bg-[#ededed]">
-          <Navbar />
-          {children}
-        </section>
+        <NextAuthProvider session={Session}>
+          <section className="grid grid-cols-12 w-full h-screen bg-[#ededed]">
+            <Navbar />
+            {children}
+          </section>
+        </NextAuthProvider>
       </body>
     </html>
   );
