914f3de0ec98c6ff45148c096c4b7386e6f4714b
Anicka <anna.jungmannova@ssps.cz>
2023-10-27 18:00:41 +0200

    feat: add sign in and sign out

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -9,12 +9,8 @@ import {
   FaUserMinus,
 } from "react-icons/fa";
 import { MdAddBox } from "react-icons/md";
-
 import Player from "./player";
-import { useEffect, useState } from "react";
-import axios from "axios";
-
-type Login = "Logged In" | "Logged Out";
+import { signOut, useSession } from "next-auth/react";
 
 const linksData = [
   {
@@ -47,37 +43,7 @@ const linksData = [
 ];
 
 const Navbar = () => {
-  const [isLogged, setIsLogged] = useState<Login>("Logged Out");
-  //   useEffect(() => {
-  //     axios
-  //       .post("http://localhost:8080/api/login/auth", "hii", {
-  //         withCredentials: true,
-  //       })
-  //       .then((res) => {
-  //         if (res.data === "Logged In") {
-  //           setIsLogged("Logged In");
-  //         } else {
-  //           setIsLogged("Logged Out");
-  //         }
-  //       })
-  //       .catch((err) => console.log(err));
-  //   }, [isLogged]);
-
-  //   const handleClick = () => {
-  //     axios
-  //       .post("http://localhost:8080/api/logout", "hii", {
-  //         withCredentials: true,
-  //       })
-  //       .then((res) => {
-  //         alert(res.data);
-  //         if (res.data === "Logged Out") {
-  //           setIsLogged("Logged Out");
-  //         } else {
-  //           setIsLogged("Logged In");
-  //         }
-  //       })
-  //       .catch((err) => console.log(err));
-  //   };
+  const { status } = useSession();
 
   return (
     <div className="w-full hidden lg:visible lg:col-span-2 bg-[#ededed] lg:flex flex-col">
@@ -90,13 +56,13 @@ const Navbar = () => {
         ))}
       </div>
       <Player />
-      {isLogged === "Logged Out" ? (
+      {status === "authenticated" ? (
         <Link href={"/login"} className="flex gap-4 m-8 mt-20">
           <FaUserPlus className="text-3xl fill-gray-600" />
           <p>Login</p>
         </Link>
       ) : (
-        <div onClick={() => console.log("hi")}>
+        <div onClick={() => signOut()}>
           <Link href={"/"} className="flex gap-4 m-8">
             <FaUserMinus className="text-3xl fill-gray-600 mt-20" />
             <p>Logout</p>
