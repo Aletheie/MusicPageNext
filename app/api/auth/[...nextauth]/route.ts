b42b1431ebb060b4118cea854e9b4bbc47e63eee
Anicka <anna.jungmannova@ssps.cz>
2023-10-27 17:26:26 +0200

    feat: add nextAuth setup

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from "@prisma/client";
+import NextAuth from "next-auth";
+import bcrypt from "bcrypt";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const prisma = new PrismaClient();
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "email", type: "email" },
+        name: { label: "name", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(
+        credentials: Record<"name" | "email" | "password", string> | undefined
+      ) {
+        const { email, password } = credentials || {};
+        if (!email || !password) {
+          return null;
+        }
+        const user = await prisma.user.findUnique({
+          where: { email },
+        });
+        if (user && (await bcrypt.compare(password, user.passwordHash))) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+};
+
+export const authHandler = NextAuth(authOptions);
+export { authHandler as GET, authHandler as POST };
