008e8d38cb27bcff73f1e611d0a10fc0b6255cd6
Anicka <anna.jungmannova@ssps.cz>
2023-10-28 21:04:22 +0200

    feat: add funcionality to play music

--- app/components/track.tsx
+++ app/components/track.tsx
@@ -2,9 +2,9 @@ import { useEffect, useState } from "react";
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 import { BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
 import { SongType } from "../../utils/song-type";
-//import useSound from "use-sound";
-import axios from "axios";
-//import useMusicStore from "../stores/musicStore";
+import useMusicStore from "@/stores/music-store";
+import useSound from "use-sound";
+//import axios from "axios";
 
 interface Props {
   song: SongType;
@@ -15,14 +15,14 @@ const Track = ({ song }: Props) => {
   const [isFilledHeart, setIsFilledHeart] = useState(
     song?.isFilledHeart || false
   );
-  //const [play, { pause }] = useSound(song.songFile.path);
-  //   const { setSong, setIsGlobalPlaying, isGlobalPlaying } = useMusicStore(
-  //     (s) => ({
-  //       setSong: s.setSong,
-  //       setIsGlobalPlaying: s.setIsGlobalPlaying,
-  //       isGlobalPlaying: s.isGlobalPlaying,
-  //     })
-  //   );
+  const [play, { pause }] = useSound(song.songFile.path);
+  const { setSong, setIsGlobalPlaying, isGlobalPlaying } = useMusicStore(
+    (s) => ({
+      setSong: s.setSong,
+      setIsGlobalPlaying: s.setIsGlobalPlaying,
+      isGlobalPlaying: s.isGlobalPlaying,
+    })
+  );
 
   //   useEffect(() => {
   //     if (!isGlobalPlaying) {
@@ -31,35 +31,35 @@ const Track = ({ song }: Props) => {
   //     }
   //   }, [isGlobalPlaying]);
 
-  //   const handlePlayPauseClick = () => {
-  //     if (isPlaying) {
-  //       setSong(song);
-  //       pause();
-  //       setIsPlaying(false);
-  //       setIsGlobalPlaying();
-  //     } else {
-  //       setSong(song);
-  //       play();
-  //       setIsPlaying(true);
-  //       setIsGlobalPlaying();
-  //     }
-  //   };
+  const handlePlayPauseClick = () => {
+    if (isPlaying) {
+      setSong(song);
+      pause();
+      setIsPlaying(false);
+      setIsGlobalPlaying();
+    } else {
+      setSong(song);
+      play();
+      setIsPlaying(true);
+      setIsGlobalPlaying();
+    }
+  };
 
   const handleHeartClick = () => {
     setIsFilledHeart(!isFilledHeart);
-    if (song) {
-      axios
-        .put(
-          "http://localhost:8080/songs/",
-          {
-            songId: song?._id,
-            isFilledHeart: !isFilledHeart,
-          },
-          { withCredentials: true }
-        )
-        .then((res) => console.log(res))
-        .catch((err) => console.log(err));
-    }
+    // if (song) {
+    //   axios
+    //     .put(
+    //       "http://localhost:8080/songs/",
+    //       {
+    //         songId: song?._id,
+    //         isFilledHeart: !isFilledHeart,
+    //       },
+    //       { withCredentials: true }
+    //     )
+    //     .then((res) => console.log(res))
+    //     .catch((err) => console.log(err));
+    // }
   };
 
   return (
@@ -68,12 +68,12 @@ const Track = ({ song }: Props) => {
         {isPlaying ? (
           <BsFillPauseFill
             className="text-4xl text-[#ededed] hover:text-[#e7e7e7]"
-            // onClick={handlePlayPauseClick}
+            onClick={handlePlayPauseClick}
           />
         ) : (
           <BsFillPlayFill
             className="text-4xl text-[#ededed] hover:text-[#e7e7e7]"
-            // onClick={handlePlayPauseClick}
+            onClick={handlePlayPauseClick}
           />
         )}
       </div>
