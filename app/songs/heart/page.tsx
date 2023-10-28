6f0493ba5915cc8df316080331f82c9e3bf99ed6
Anicka <anna.jungmannova@ssps.cz>
2023-10-28 20:19:53 +0200

    feat: disable heart page

--- app/songs/heart/page.tsx
+++ app/songs/heart/page.tsx
@@ -1,39 +1,25 @@
 "use client";
 
-import axios from "axios";
-import { useEffect, useState } from "react";
-import Track from "@/app/components/track";
-import { SongType } from "@/utils/song-type";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 
 const HeartPage = () => {
-  const [songHeartList, setSongHeartList] = useState<SongType[]>([]);
-
-  //   useEffect(() => {
-  //     axios
-  //       .post<SongType[]>("http://localhost:8080/songs/heart", "hii", {
-  //         withCredentials: true,
-  //       })
-  //       .then((res) => setSongHeartList(res.data))
-  //       .catch((err) => console.log(err));
-  //   }, []);
-
+  const { status } = useSession();
   return (
     <div className="w-full h-screen col-span-12 lg:col-span-10 bg-[#f4f4f4] rounded-l-3xl shadow-md overflow-hidden">
       <div className="w-2/3 h-screen mx-auto flex justify-center items-center flex-col">
         <h1 className="text-gray-700 font-bold text-5xl pb-5">
           All {"\u003C"}3 Songs
         </h1>
-        {songHeartList.map((song, idx) => (
-          <Track key={idx} song={song} />
-        ))}
-        {songHeartList.length === 0 && (
+        <p className="text-gray-700 font-semibold text-2xl">
+          This feature will be added soon 🫣
+        </p>
+        {status === "unauthenticated" && (
           <p className="text-gray-700 font-semibold text-2xl">
-            No songs found. You can add them{" "}
-            <Link href="/songs/add" className="text-blue-500">
+            You can login{" "}
+            <Link href="/login" className="text-blue-500">
               here
             </Link>
-            .
           </p>
         )}
       </div>
