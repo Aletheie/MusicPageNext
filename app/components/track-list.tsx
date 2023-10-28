c3fbeb6b4c037757b039bcf787ab5e7f39c96379
Anicka <anna.jungmannova@ssps.cz>
2023-10-28 21:04:08 +0200

    feat: make simple example

--- app/components/track-list.tsx
+++ app/components/track-list.tsx
@@ -1,29 +1,49 @@
 "use client";
 
-import { use, useEffect, useState } from "react";
+import { useEffect } from "react";
 import Track from "./track";
 import Link from "next/link";
-import axios from "axios";
-import { useSession } from "next-auth/react";
+// import axios from "axios";
+// import { useSession } from "next-auth/react";
 import useMusicStore from "@/stores/music-store";
 import { SongType } from "@/utils/song-type";
 
 const TrackList = () => {
-  const { data, status } = useSession();
+  //const { data, status } = useSession();
   const { setGlobalSongList, globalSongList } = useMusicStore((s) => ({
     setGlobalSongList: s.setGlobalSongList,
     globalSongList: s.globalSongList,
   }));
 
+  const defaultSong: SongType = {
+    songName: "Ukázková skladba",
+    songAuthor: "forest lullaby",
+    _id: "uhfoiwrubfi",
+    isFilledHeart: false,
+    songFile: {
+      _id: "uhfoiwrubfi",
+      duration: 0,
+      bitRate: 0,
+      format: "",
+      name: "Ukázková skladba",
+      path: "https://res.cloudinary.com/dgj3s3q6m/video/upload/v1698518884/MusicPage/forest-lullaby-110624_ugrsfv.mp3",
+    },
+  };
+
   useEffect(() => {
-    axios
-      .post<SongType[]>("http://localhost:3000/api/songs", { status, data })
-      .then((res) => {
-        console.log(res.data);
-        setGlobalSongList(res.data);
-      })
-      .catch((err) => console.log(err));
-  }, [data, status, setGlobalSongList]);
+    setGlobalSongList([defaultSong]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [setGlobalSongList]);
+
+  // useEffect(() => {
+  //   axios
+  //     .post<SongType[]>("http://localhost:3000/api/songs", { status, data })
+  //     .then((res) => {
+  //       console.log(res.data);
+  //       setGlobalSongList(res.data);
+  //     })
+  //     .catch((err) => console.log(err));
+  // }, [data, status, setGlobalSongList]);
 
   return (
     <div className=" text-left mx-10 md:ml-[55%] mt-16 font-bold text-3xl w-full md:w-[60%] lg:w-[45%] ">
