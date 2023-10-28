a2f69b479838d6b64cf986a72408737f953da32a
Anicka <anna.jungmannova@ssps.cz>
2023-10-28 18:42:09 +0200

    fix: delete middlewares

--- middlewares/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { checkSongLimit } from "./check-song-limit";
-import { stackMiddlewares } from "@/middlewares/stack-handler";
-
-export const config = {
-  matcher: "/api/songs",
-};
-
-const middlewares = [checkSongLimit];
-export default stackMiddlewares(middlewares);
